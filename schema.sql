624bc0c531966db575c5637091b636927f92c054
John Manack <jmnck28@gmail.com>
2021-03-16 17:05:25 -0400

    functioning schema and seed data to create db; currently running npm db:reset creates a full functioning db

---
+++ schema.sql
@@ -0,0 +1,34 @@
+CREATE TABLE user_info (
+    id serial PRIMARY KEY,
+    first_name varchar(200),
+    last_name varchar(200),
+    email varchar (200),
+    password varchar(2000)
+);
+
+CREATE TABLE activities (
+    id serial PRIMARY KEY,
+    title varchar(2000),
+    details varchar(10000),
+    is_complete boolean,
+    is_billable boolean,
+    user_reference integer REFERENCES user_info (id)
+);
+
+CREATE TABLE nested_notes (
+    id serial PRIMARY KEY,
+    note_entry varchar (10000),
+    activity_reference integer REFERENCES activities (id)
+);
+
+CREATE TABLE nested_hours (
+    id serial PRIMARY KEY,
+    hours_entry integer,
+    hours_description varchar(2000),
+    activity_reference integer REFERENCES activities (id)
+);
+
+CREATE TABLE user_favorites (
+    id serial PRIMARY KEY,
+    activity_reference integer REFERENCES activities (id)
+);
